20250038808fe80350ab8b08fb70c8fa12c4beba
dikimeip <pm159145@gmail.com>
2019-12-09 09:04:25 +0700

    memecah file template

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -10,6 +10,9 @@ class UserController extends CI_Controller
 	public function index()
 	{
 		$this->load->view('template/header');
+		$this->load->view('template/menu');
+		$this->load->view('user/dasboard');
+		$this->load->view('template/footer');
 	}
 
 }
---
+++ application/views/template/footer.php
@@ -0,0 +1,46 @@
+    </div>
+         </div>
+            <!-- ============================================================== -->
+            <!-- end pageheader  -->
+            <!-- ============================================================== -->
+        </div>
+        </div>
+            <!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+           
+            <!-- ============================================================== -->
+            <!-- end footer -->
+            <!-- ============================================================== -->
+        </div>
+        <!-- ============================================================== -->
+        <!-- end wrapper  -->
+        <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- end main wrapper  -->
+    <!-- ============================================================== -->
+    <!-- Optional JavaScript -->
+    <!-- jquery 3.3.1 -->
+    <script src="<?php echo base_url() ?>asset/vendor/jquery/jquery-3.3.1.min.js"></script>
+    <!-- bootstap bundle js -->
+    <script src="<?php echo base_url() ?>asset/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+    <!-- slimscroll js -->
+    <script src="<?php echo base_url() ?>asset/vendor/slimscroll/jquery.slimscroll.js"></script>
+    <!-- main js -->
+    <script src="<?php echo base_url() ?>asset/libs/js/main-js.js"></script>
+    <!-- chart chartist js -->
+    <script src="<?php echo base_url() ?>asset/vendor/charts/chartist-bundle/chartist.min.js"></script>
+    <!-- sparkline js -->
+    <script src="<?php echo base_url() ?>asset/vendor/charts/sparkline/jquery.sparkline.js"></script>
+    <!-- morris js -->
+    <script src="<?php echo base_url() ?>asset/vendor/charts/morris-bundle/raphael.min.js"></script>
+    <script src="<?php echo base_url() ?>asset/vendor/charts/morris-bundle/morris.js"></script>
+    <!-- chart c3 js -->
+    <script src="<?php echo base_url() ?>asset/vendor/charts/c3charts/c3.min.js"></script>
+    <script src="<?php echo base_url() ?>asset/vendor/charts/c3charts/d3-5.4.0.min.js"></script>
+    <script src="<?php echo base_url() ?>asset/vendor/charts/c3charts/C3chartjs.js"></script>
+    <script src="<?php echo base_url() ?>asset/libs/js/dashboard-ecommerce.js"></script>
+</body>
+ 
+</html>
