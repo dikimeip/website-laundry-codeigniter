2b9fe99823a42ddfddec1b823953803bd85b3fab
dikimeip <pm159145@gmail.com>
2019-12-09 08:30:10 +0700

    edit menu and header template

---
+++ application/controllers/AdminController.php
@@ -0,0 +1,10 @@
+<?php  
+
+/**
+ * 
+ */
+class AdminController extends CI_Controller
+{
+	
+	
+}
---
+++ application/controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php  
+
+
+/**
+ * 
+ */
+class UserController extends CI_Controller
+{
+	
+	public function index()
+	{
+		$this->load->view('template/header');
+	}
+
+}
