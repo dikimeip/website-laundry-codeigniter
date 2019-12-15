1aa01695e65cde3c5f7ff5c741c009176da39f8f
dikimeip <pm159145@gmail.com>
2019-12-15 14:38:05 +0700

    membuat halaman dasboard

--- application/views/admin/dasboard.php
+++ application/views/admin/dasboard.php
@@ -1,1 +1,28 @@
 <h1>ADMIN DASBOARD <?php echo date('Y-m-d') ?> </h1>
+<br><hr>
+<div class="row">
+	<div class="col-md-4">
+		<div class="alert alert-info">
+			<p class="text-center"><b>BARANG</b></p>
+			<p>Jumlah Produk :  Barang</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="alert alert-info">
+			<p class="text-center"><b>BARANG</b></p>
+			<p>Jumlah Produk :  Barang</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="alert alert-info">
+			<p class="text-center"><b>BARANG</b></p>
+			<p>Jumlah Produk :  Barang</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="alert alert-info">
+			<p class="text-center"><b>BARANG</b></p>
+			<p>Jumlah Produk :  Barang</p>
+		</div>
+	</div>
+</div>
