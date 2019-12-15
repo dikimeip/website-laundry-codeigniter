8b46cbc0d68ebe139fd626e4dcb3464ea1ef83dd
dikimeip <pm159145@gmail.com>
2019-12-15 14:46:11 +0700

    memperbaiki halaman dasboard user

--- application/views/user/dasboard.php
+++ application/views/user/dasboard.php
@@ -1,2 +1,28 @@
 
 <h1>DASBOARD MENU </h1>
+<div class="row">
+	<div class="col-md-4">
+		<div class="alert alert-info">
+			<p class="text-center"><b>TRANSAKSI</b></p>
+			<p>Jumlah Produk : <?php echo count($trans) ?>  Barang</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="alert alert-info">
+			<p class="text-center"><b>PAKET</b></p>
+			<p>Jumlah Produk : <?php echo count($pakets) ?> Barang</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="alert alert-info">
+			<p class="text-center"><b>USER</b></p>
+			<p>Jumlah Produk : <?php echo count($user) ?> Barang</p>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="alert alert-info">
+			<p class="text-center"><b>KARYAWAN</b></p>
+			<p>Jumlah Produk : <?php echo count($karyawan) ?> Barang</p>
+		</div>
+	</div>
+</div>
